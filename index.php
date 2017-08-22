7d2a37562f521d3ea4f394a1975428a90970ed05
inusaico90 <erneyjj@hotmail.es>
2017-08-22 15:44:16 -0500

    delet

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$ip = $_SERVER['REMOTE_ADDR']; 
-$f = fopen("ip.html", "a"); 
-fwrite ($f, 'IP: [<b><font color="#005700">'.$ip.'</font></b>]<br>');
-fclose($f);
-
-header("Location: index2.html");
-?>
